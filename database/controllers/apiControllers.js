b822026236e6d05a3818716073824179f8c8dcdf
benjaminpettus <benjaminpettus@gmail.com>
2017-04-19 14:56:12 -0700

    WIP zipToLatLng working

--- database/controllers/apiControllers.js
+++ database/controllers/apiControllers.js
@@ -19,9 +19,30 @@ const apiControllers = {
         return request(options)
             .then(response => {
                 return response.zip_codes[0]})
-            .catch( error => error )
+            .catch( error => {
+              throw error
+            })
       },
+      zipToLatLng: input => {
+        const zip = parseInt(input) ? parseInt(input) : null
+        const options = {
+            method: 'GET',
+            uri: encodeURI(`http://www.zipcodeapi.com/rest/info.json/${zip}/degrees`),
+            qs: {
+                access_token: majorKeys.zipKeyJS
+            },
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        }
+        return request(options)
+            .then(response => {
+                return {lat: response.lat, lng: response.lng}
+            })
+      }
 
 }
 
 export default apiControllers
+apiControllers.zipToLatLng(94529).then(response => {console.log(response)})
--- tests/apiTests/apiController_test.js
+++ tests/apiTests/apiController_test.js
@@ -2,17 +2,18 @@ import nock from 'nock'
 import chai from 'chai'
 import { default as chaiHttp } from 'chai-http'
 import { expect } from 'chai'
+import { majorKeys } from '../../server/keys'
 const should = chai.should()
 
 chai.use( chaiHttp )
 
 import { apiControllers } from '../../database/controllers'
 
-const urlDomain = 'http://www.zipcodeapi.com'
-const urlPath = '/rest/city-zips.json/Los%20Angeles/ca'
-const reqHeaders = { reqHeaders: { 'User-Agent': 'Request-Promise', 'Host': 'www.zipcodeapi.com', 'Accept': 'application/json' } }
+
 
 describe('\napiControllers\n', () => {
+  let urlDomain = 'http://www.zipcodeapi.com'
+  let urlPath = '/rest/city-zips.json/Los%20Angeles/ca'
 
   context('nameToZip()', () => {
     nock( urlDomain  )
@@ -29,16 +30,82 @@ describe('\napiControllers\n', () => {
 
     nock( urlDomain  )
       .get( '/rest/city-zips.json/Los%20Angeles/vvv' )
-      .query({ access_token: 'js-L3CZB6qGQTdETLM6OCZbiHmIexaFSOJDRmDts39iWnXQ9JWXmW9EDvnS2xxVmhye' })
+      .query({ access_token: majorKeys.zipKeyJS })
       .reply( 503, 'Invalid City' )
 
     it( 'returns an error when provided invalid input.', () => {
         apiControllers.nameToZip('Los Angeles', 'vvv')
-            .then( response => {
+            .catch( response => {
                 expect( response.name ).to.eql( 'StatusCodeError' )
                 expect( response.message ).to.eql( '503 - "Invalid City"' )
             })
     })
 
     })
+})
+
+describe('zipToLatLng', () => {
+  let urlDomain = 'http://www.zipcodeapi.com'
+  let urlPath = '/rest/info.json/94529/degrees'
+  context('takes zip code and returns coordinates', () => {
+    nock( urlDomain )
+      .get(urlPath)
+      .query({ access_token: majorKeys.zipKeyJS })
+      .reply(200, {
+        "zip_code": "94529",
+        "lat": 37.869991,
+        "lng": -122.040017,
+        "city": "Concord",
+        "state": "CA",
+        "timezone": {
+           "timezone_identifier": "America/Los_Angeles",
+           "timezone_abbr": "PDT",
+           "utc_offset_sec": -25200,
+           "is_dst": "T"
+        },
+        "acceptable_city_names": [
+           {
+               "city": "Chevron",
+               "state": "CA"
+           },
+           {
+               "city": "Chevron Usa Inc",
+               "state": "CA"
+           }
+        ]
+})
+    it('returns coordinates object with Lat, Lng properties', () => {
+      apiControllers.zipToLatLng(94529)
+        .then( response => {
+          expect( response.lat ).to.eql(37.869991)
+          expect( response.lng ).to.eql(-122.040017)
+        })
+    })
+    nock( urlDomain )
+      .get(urlPath)
+      .query({ access_token: majorKeys.zipKeyJS })
+      .reply(404, {
+        "error_code": 400,
+        "error_msg": "Invalid request."
+      })
+    it('should thow an error if input is NaN', () => {
+      apiControllers.zipToLatLng('not a zip')
+        .catch( error => {
+          expect(error).to.not.be.null
+        })
+    })
+    nock( urlDomain )
+      .get(urlPath)
+      .query({ access_token: majorKeys.zipKeyJS })
+      .reply(404, {
+         "error_code": 404,
+         "error_msg": "Zip code \"99997\" not found."
+       })
+    it('should return an error if given invalid zip', () => {
+      apiControllers.zipToLatLng(99997)
+        .catch( error => {
+          expect(error).to.not.be.null
+        })
+    })
+  })
 })
